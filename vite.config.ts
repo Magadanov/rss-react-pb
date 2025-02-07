8cd06db08d0a87a4368add816d73c5514dbc4934
Magadanov <magadanov03@mail.ru>
2025-02-07 22:16:34 +0500

    feat: add gh-pages lib

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rss-react-pb/hooks-and-routing',
+  build: {
+    outDir: 'dist/hooks-and-routing',
+  },
 });
