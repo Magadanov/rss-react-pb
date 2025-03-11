465d8c63903a4304c4bbf051eaa3b30693af2961
Magadanov <magadanov03@mail.ru>
2025-03-11 22:05:39 +0500

    fix: fix deploy

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/1',
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
-  distDir: './dist/nextjs-ssr-app-router-api',
-  basePath: '/rss-react-pb/nextjs-ssr-app-router-api',
-};
+const nextConfig = {};
 
 export default nextConfig;
