d1b13644200a56b38c3ff25d1682830efaaf37f2
Magadanov <magadanov03@mail.ru>
2025-03-11 22:14:06 +0500

    fix: fix deploy

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,5 @@ import type { Config } from '@react-router/dev/config';
 
 export default {
   appDirectory: 'src',
-  basename: '/rss-react-pb/react-router-ssr',
   ssr: true,
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [reactRouter()],
-  base: '/rss-react-pb/react-router-ssr',
-  build: {
-    outDir: 'dist/react-router-ssr',
-  },
 });
